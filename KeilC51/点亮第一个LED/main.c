4f917acabd6b0dccb438050fde3ee57faad494ed
HeL1x-C <bronyazaychik16c@outlook.com>
2026-09-22 14:32:25 +0800

    add stdint.h

--- KeilC51/点亮第一个LED/main.c
+++ KeilC51/点亮第一个LED/main.c
@@ -1,5 +1,5 @@
 #include"REG51.H"
-
+#include"stdint.h"
 
 sbit led1 = P2^0; // Define led1 as the first bit of port 2
 sbit led2 = P2^1; // Define led2 as the second bit of port 2
@@ -9,8 +9,12 @@ sbit led5 = P2^4; // Define led5 as the fifth bit of port 2
 sbit led6 = P2^5; // Define led6 as the sixth bit of port 2
 sbit led7 = P2^6; // Define led7 as the seventh bit of port 2
 
-typedef unsigned char u8;
-typedef unsigned int u16;
+//typedef unsigned char u8;
+//typedef unsigned int u16;    原写法
+
+
+typedef uint8_t u8;
+typedef uint16_t u16;    //加入头文件stdint.h以后的写法
 
 
 void delay10ms(u16 ten_us)
