75f334b1bda9bfb06c2abd4f35c0ebd5198c2d99
HeL1x-C <bronyazaychik16c@outlook.com>
2026-09-24 09:49:53 +0800

    fix some bugs

--- KeilC51/Finished 点亮第一个LED/main.c
+++ KeilC51/Finished 点亮第一个LED/main.c
@@ -1,20 +1,20 @@
 #include<REG52.H>
 //#include<stdint.h>
 
-sbit led1 = P2^0; // Define led1 as the first bit of port 2
-sbit led2 = P2^1; // Define led2 as the second bit of port 2
-sbit led3 = P2^2; // Define led3 as the third bit of port 2
-sbit led4 = P2^3; // Define led4 as the fourth bit of port 2
-sbit led5 = P2^4; // Define led5 as the fifth bit of port 2
-sbit led6 = P2^5; // Define led6 as the sixth bit of port 2
-sbit led7 = P2^6; // Define led7 as the seventh bit of port 2
+sbit led1 = P2^0;
+sbit led2 = P2^1;
+sbit led3 = P2^2;
+sbit led4 = P2^3;
+sbit led5 = P2^4;
+sbit led6 = P2^5;
+sbit led7 = P2^6;
 
 typedef unsigned char u8;
 typedef unsigned int u16;    //原写法
 
 
 //typedef uint8_t u8;
-//typedef uint16_t u16;    //加入头文件stdint.h以后的写法,但是很遗憾C51不支持c99
+//typedef uint16_t u16;    //加入头文件stdint.h以后的写法,但是很遗憾烧写过程中发现C51不支持c99语法
 
 
 void delay10ms(u16 ten_us)
@@ -26,12 +26,13 @@ void main()
 {
     while(1)
     {
-        led1 = 0; // Turn on led1
-        led2 = 0; // Turn on led2
-        led3 = 0; // Turn on led3
-        led4 = 0; // Turn on led4
-        led5 = 0; // Turn on led5
-
+        led1 = 0;
+        led2 = 0;
+        led3 = 0;
+        led4 = 0;
+        led5 = 0;
+        led6 = 0;
+        led7 = 0;
     }
 
     
--- KeilC51/静态数码管/main.c
+++ KeilC51/静态数码管/main.c
@@ -3,9 +3,19 @@
 typedef unsigned char u8;
 typedef unsigned int u16;
 
+sbit LED1 = P2^0;
+
+void delay_10us(u16 ten_us)
+{
+    while(ten_us--);
+}
+
+
 void main()
 {
-
-
+    while(1)
+    {
+        delay_10us(50000);
+    }
 
 }
